0c6606cf6c6e399c6db14d845184b99ffad7fd82
Jason White <jasonadal354@gmail.com>
2022-11-07 17:08:52 -0500

    finished implementing all questions and answers in Javascript

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ const questions = [{
             ]
         },
         {
-        id: 1,
+            q: 1,
         question: "Zavala had a family centuries prior to the present day, his wife was named Safiya, and his son's name was:",
         answer: [{text: "Gabriel", isCorrect: false},
                     {text: "Adal", isCorrect: false},
@@ -33,7 +33,7 @@ const questions = [{
                 ]
             },
             {
-                id: 2,
+                q: 2,
                 question: "Who oversought the Trials of Osiris in Destiny 1?",
                 answer: [{text: "Saint-14", isCorrect: false},
                             {text: "Brother Vance", isCorrect: true},
@@ -42,23 +42,70 @@ const questions = [{
                         ]
                     },
                 {
-                    id: 3, 
+                    q: 3, 
                     question: "In the Vow of the Disciple raid, what is considered the first boss encounter?",
                     answer: [{text: "The Caretaker", isCorrect: true},
                                 {text: "Rhulk, the First Disciple", isCorrect: false},
                                 {text: "The Winnower", isCorrect: false},
                                 {text: "The Crunchwrap Supreme", isCorrect: false}]
                 },
-            
-                
-                
-                "Which Destiny character personally favorited the Spicy Ramen Shop?",
-                "Who is the most useless character listed in the roster?",
-                "This Omolon Exotic weapon plagued the crucible during Season of the Worthy due to the season's buff to Auto Rifles:",
-                "This Exotic Chest piece for the Titan Class amplifies the amount of damage Thundercrash can output: ",
-                "What is the name of Elsie Bray's (The Exo Stranger) fish like companion?",
-                "Finish the phrase: Whether we wanted it or not... "
-                ];
+                {
+                    q: 4, 
+                    question: "Which Destiny character personally favorited the Spicy Ramen Shop?",
+                    answer: [{text: "Zavala", isCorrect: false},
+                                {text: "Ikora Rey", isCorrect: false},
+                                {text: "Oryx, The Taken King", isCorrect: false},
+                                {text: "Cayde-6", isCorrect: true}]
+                },
+                {
+                    q: 5, 
+                    question: "According to the Destiny community, who USED TO BE the most useless character listed in the roster?",
+                    answer: [{text: "Ikora Rey", isCorrect: true},
+                                {text: "Cayde-6", isCorrect: false},
+                                {text: "Mithrax", isCorrect: false},
+                                {text: "The Exo Stranger", isCorrect: false}]
+                },
+                {
+                    q: 6, 
+                    question: "According to the Destiny community, who USED TO BE the most useless character listed in the roster?",
+                    answer: [{text: "Ikora Rey", isCorrect: true},
+                                {text: "Cayde-6", isCorrect: false},
+                                {text: "Mithrax", isCorrect: false},
+                                {text: "The Exo Stranger", isCorrect: false}]
+                },
+                {
+                    q: 7, 
+                    question: "This Omolon Exotic weapon plagued the crucible during Season of the Worthy due to the season's buff to Auto Rifles:",
+                    answer: [{text: "The Suros Regime", isCorrect: false},
+                                {text: "Sweet Business", isCorrect: false},
+                                {text: "Lorentz Driver", isCorrect: false},
+                                {text: "Hard Light", isCorrect: true}]
+                },
+                {
+                    q: 8, 
+                    question: "This exotic chest piece for the Titan Class amplifies the amount of damage Thundercrash can output: ",
+                    answer: [{text: "Heart of Inmost Light", isCorrect: false},
+                                {text: "Armementarium", isCorrect: false},
+                                {text: "Peacekeepers", isCorrect: false},
+                                {text: "Cuirass of the Falling Star", isCorrect: true}]
+                },
+                {
+                    q: 9, 
+                    question: "What is the name of Elsie Bray's (The Exo Stranger) fish like companion?",
+                    answer: [{text: "Chuckie", isCorrect: false},
+                                {text: "Pookah", isCorrect: true},
+                                {text: "Nemo", isCorrect: false},
+                                {text: "Louis", isCorrect: false}]
+                },
+                {
+                    q: 10, 
+                    question: "Finish the phrase: Whether we wanted it or not... ",
+                    answer: [{text: "we've stepped into a war with the Cabal on Mars.", isCorrect: true},
+                                {text: "we've inserted ourselves right into the middle of an intergalactic onslaught.", isCorrect: false},
+                                {text: "we're getting pineapple on the pizza.", isCorrect: false},
+                                {text: "we've started the Sword Logic ritual for the one person we did not want to start it for.", isCorrect: false}]
+                },
+            ];
 
 // When rendering question, it should also render answers in buttons
 
